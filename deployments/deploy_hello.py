bfdbd19064ec392839d9b6603298982dc445a5be
Graziela <graziela.cavalcante@ons.org.br>
2025-04-16 01:25:12 -0300

    correcao

--- deployments/deploy_hello.py
+++ deployments/deploy_hello.py
@@ -4,6 +4,7 @@ if __name__ == "__main__":
     hello_world.deploy(
         name="hello-every-30min",
         schedule="*/30 * * * *",
+        work_pool_name="default-agent-pool",  # Substitua com o nome do seu Work Pool
         tags=["demo"],
     )
 
