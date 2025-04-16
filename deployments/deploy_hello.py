15bc945cdad4630b2f15def773fc4f6ccc1f322a
Graziela <graziela.cavalcante@ons.org.br>
2025-04-16 01:14:15 -0300

    agendando corretamente para 30min, com .serve se autoagendava

--- deployments/deploy_hello.py
+++ deployments/deploy_hello.py
@@ -1,14 +1,9 @@
-import sys
-from pathlib import Path
-
-# Adiciona o diretório raiz ao caminho Python
-sys.path.append(str(Path(__file__).parent.parent))
-
 from flows.hello import hello_world
 
 if __name__ == "__main__":
-    hello_world.serve(
+    hello_world.deploy(
         name="hello-every-30min",
-        cron="*/30 * * * *",
-        tags=["demo"]
+        schedule="*/30 * * * *",
+        tags=["demo"],
     )
+
